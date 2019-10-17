55a9c230ca5fc951c3d849d0d8f907c1d5f3bc7d
damilola.olawumi <damilola_olawumi@yahoo.com>
2019-10-17 19:36:32 +0100

    made a few changes in the app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,55 @@
 import React, { useState } from "react";
-import { Route, Router } from "react-router-dom";
+import axios from 'axios';
+import { Route, Link } from "react-router-dom";
 import SavedList from "./Movies/SavedList";
+import MovieUpdate from "./Movies/MovieUpdate";
 import MovieList from "./Movies/MovieList";
 import Movie from "./Movies/Movie";
-import MovieUpdate from "./Movies/MovieUpdate";
-import { createBrowserHistory } from "history";
 
-export const customHistory = createBrowserHistory();
-
-const App = () => {
+const App = props => {
   const [savedList, setSavedList] = useState([]);
+  const [movies, setMovies] = useState([]);
 
   const addToSavedList = movie => {
     setSavedList([...savedList, movie]);
   };
-  
+
+  const deleteMovie = (id) => (e) => {
+
+
+    axios.delete(`http://localhost:5000/api/movies/${id}`)
+      .then(({ data }) => {
+
+        setMovies(movies.filter(movie => movie.id !== data));
+        window.location.href = "/";
+      })
+      .catch(err => console.log(err));
+
+  }
+
+
   return (
     <>
       <SavedList list={savedList} />
-      <Router history={customHistory}>
-        <Route exact path="/" component={MovieList} />
-        <Route
-          path="/movies/:id"
-          render={props => {
-            return <Movie {...props} addToSavedList={addToSavedList} />;
-          }}
-        />
-        <Route path='/update-movie/:id' component={MovieUpdate} />
-      </Router>
+      <Route
+        exact
+        path="/"
+        render={props => {
+          return <MovieList {...props} movies={movies} setMovies={setMovies} />;
+        }}
+      />
+      <Route
+        path="/movies/:id"
+        render={props => {
+          return <Movie {...props} addToSavedList={addToSavedList} deleteMovie={deleteMovie} />;
+        }}
+      />
+      <Route
+        path="/update-movie/:id"
+        render={props => (
+          <MovieUpdate {...props} movieDetails={movies} setMovies={setMovies} />
+        )}
+      />
     </>
   );
 };
